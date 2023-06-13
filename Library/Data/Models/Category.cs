acb8b5926277769015e22da3cf5912b658ea841f
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-13 20:32:08 +0300

    Implemented Category class

---
+++ Library/Data/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿//using MessagePack;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5), MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+}
