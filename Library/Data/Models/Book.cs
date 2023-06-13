dd47d4a0b39ae554bd00bf6c90c2647cc8076e83
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-13 20:29:56 +0300

    edit

--- Library/Data/Models/Book.cs
+++ Library/Data/Models/Book.cs
@@ -1,6 +1,42 @@
-﻿namespace Library.Data.Models
+﻿//using MessagePack;
+//
+//using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library.Data.Models
 {
     public class Book
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(10), MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(5), MaxLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [MinLength(5), MaxLength(5000)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        [Range(0.00, 10.00)]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+
+        public List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
     }
 }
