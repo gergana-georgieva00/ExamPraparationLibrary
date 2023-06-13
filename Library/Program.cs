77ce2f3705ea75d7590f9d420364bbb841c1f290
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-13 20:05:17 +0300

    edit

--- Library/Program.cs
+++ Library/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<LibraryDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<LibraryDbContext>();
 
 builder.Services.Configure<IdentityOptions>(options =>
