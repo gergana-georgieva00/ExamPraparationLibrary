60f80ad0e62bdf2aaab156b35f6a8073d5a7138f
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-14 12:39:43 +0300

    Edit of Program

---
+++ Library/Services/BookService.cs
@@ -0,0 +1,13 @@
+﻿using Library.Contracts;
+using Library.Models;
+
+namespace Library.Services
+{
+    public class BookService : IBookService
+    {
+        public Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
