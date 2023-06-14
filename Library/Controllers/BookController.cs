cc1dbdac1985a49cb29cd07eb93a31b96fff5a43
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-14 16:33:48 +0300

    Implemented BookViewModel

--- Library/Controllers/BookController.cs
+++ Library/Controllers/BookController.cs
@@ -12,6 +12,22 @@ namespace Library.Controllers
             this.bookService = bookService;
         }
 
+        public async Task<IActionResult> AddToCollection(int id)
+        {
+            var book = await bookService.GetBookByIdAsync(id);
+
+            if (book == null)
+            {
+                return RedirectToAction(nameof(All));
+            }
+
+            var userId = GetUserId();
+
+            await bookService.AddBookToCollectionAsync(userId, book);
+
+            return RedirectToAction(nameof(All));
+        }
+
         public async Task<IActionResult> Mine()
         {
             var model = await bookService.GetMyBooksAsync(GetUserId());
