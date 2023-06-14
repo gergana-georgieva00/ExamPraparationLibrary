fd37045ca64810c3a7eff631424adba7230ae2c6
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-14 15:23:53 +0300

    Edited All

--- Library/Controllers/BookController.cs
+++ Library/Controllers/BookController.cs
@@ -1,16 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Library.Contracts;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Controllers
 {
     public class BookController : BaseController
     {
+        private readonly IBookService bookService;
+
+        public BookController(IBookService bookService)
+        {
+            this.bookService = bookService;
+        }
+
         public IActionResult Add()
         {
             return View();
         }
 
-        public IActionResult All()
+        public async Task<IActionResult> All()
         {
+            var model = await bookService.GetAllBooksAsync();
+
             return View();
         }
     }
