d4c56efe215b11b08ddd0509e5723ece5444b0bf
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-14 12:43:27 +0300

    Implemented GetAllBooksAsync()

--- Library/Services/BookService.cs
+++ Library/Services/BookService.cs
@@ -1,13 +1,32 @@
 ﻿using Library.Contracts;
+using Library.Data;
 using Library.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library.Services
 {
     public class BookService : IBookService
     {
-        public Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync()
+        private readonly LibraryDbContext dbContext;
+
+        public BookService(LibraryDbContext dbContext)
         {
-            throw new NotImplementedException();
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync()
+        {
+            return await this.dbContext
+                .Books
+                .Select(b => new AllBookViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ImageUrl = b.ImageUrl,
+                    Rating = b.Rating,
+                    Category = b.Category.Name
+                }).ToListAsync();
         }
     }
 }
