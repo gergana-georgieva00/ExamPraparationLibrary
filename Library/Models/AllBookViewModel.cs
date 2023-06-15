89928c80f0d270b74fa6096f8c5e304cc6417ed2
Gergana Georgieva <gerganagergana1@gmail.com>
2023-06-15 14:27:16 +0300

    edit

--- Library/Models/AllBookViewModel.cs
+++ Library/Models/AllBookViewModel.cs
@@ -15,7 +15,8 @@ namespace Library.Models
 
         public string ImageUrl { get; set; } = null!;
 
-        public decimal Rating { get; set; }
+        public decimal? Rating { get; set; }
+
         public string? Description { get; set; }
 
         public string Category { get; set; } = null!;
